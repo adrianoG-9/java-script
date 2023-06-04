cce9f269b35dc6bad2ca5945a61d2f9ba104fa74
adrianoG-9 <127891321+adrianoG-9@users.noreply.github.com>
2023-06-04 19:21:41 -0300

    atualização de exc condições JS

---
+++ guanabara/exercicios/condições_javascript/condições.js
@@ -0,0 +1,14 @@
+
+let vel=30
+if(vel>60){
+    console.log(`porra.. ${vel}Km/h!? você foi MULTADO seu merda!`)
+}
+else{
+    console.log(`A velocidade de sua carroça é ${vel} Km/h , parabéns`)
+}
+.exit
+
+
+
+
+
